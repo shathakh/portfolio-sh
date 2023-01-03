7b3bef7bec629064683326cde2b7d2209cb5aaa4
shathakh <shathaqudaih17@gmail.com>
2023-01-03 09:06:41 +0200

    make responsive navbar

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   const [fixed, setFix] = useState(false);
+  const [clicked, setClick] = useState(false);
 
   const setFixed = () => {
     if (window.scrollY > 300) setFix(true);
     else setFix(false);
+  };
+
+  const handelClick = () => {
+    setClick(!clicked);
   };
 
   window.addEventListener('scroll', setFixed);
@@ -14,14 +21,16 @@ function Header() {
     <div className={fixed ? 'scrolling' : 'header'}>
       <div className="logo">ShathaKh</div>
       <div className="content-list">
-        <ul className="navbar">
+        <ul className={clicked ? 'navbar active' : 'navbar'}>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contacts">Contacts</a></li>
         </ul>
       </div>
-      
+      <div id="mobile">
+        <FontAwesomeIcon onClick={handelClick} className="bar-icon" icon={clicked ? faTimes : faBars} />
+      </div>
     </div>
   );
 }
